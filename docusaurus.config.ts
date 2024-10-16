0907de9caedb17211d95142ee112a4f2bc551012
Haylee Schäfer <haylee@inventivetalent.org>
2024-10-16 22:58:29 +0200

    dark default

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -64,6 +64,10 @@ const config: Config = {
   themeConfig: {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
+    colorMode: {
+        defaultMode: 'dark',
+        disableSwitch: false
+    },
     navbar: {
       title: 'MineSkin Docs',
       logo: {
