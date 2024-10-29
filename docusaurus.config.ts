2ec16fe7a7615dc0093c0484906eee19a0d89bf6
Haylee Schäfer <haylee@inventivetalent.org>
2024-10-29 11:45:21 +0100

    replace social card

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -66,7 +66,7 @@ const config: Config = {
 
     themeConfig: {
         // Replace with your project's social card
-        image: 'img/docusaurus-social-card.jpg',
+        image: 'img/mineskin-social-card-docs.jpg',
         colorMode: {
             defaultMode: 'dark',
             disableSwitch: false
